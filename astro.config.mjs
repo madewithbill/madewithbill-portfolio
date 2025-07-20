dd2dde8612c1d776cda3ea25b809b6766f5d80d8
madewithbill <165756396+madewithbill@users.noreply.github.com>
2025-07-19 21:08:03 -0700

    Corrected false import in Astro config. Added noopener to Footer component.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 import sitemap from "@astrojs/sitemap";
-import { never } from "astro:schema";
 
 // https://astro.build/config
 export default defineConfig({

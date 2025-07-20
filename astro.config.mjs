a6c06989e567186cf8a8b35d1c5c714bdc1cd3d1
madewithbill <165756396+madewithbill@users.noreply.github.com>
2025-07-19 21:50:32 -0700

    Removed slash config due to Pages rules. Applied dynamic canonicals to base component. Pushing for staging test.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.madewithbill.com",
-  trailingSlash: "never",
+  // trailingSlash: "never",
   vite: {
     plugins: [tailwindcss()],
   },

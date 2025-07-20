3f9ab4d28a5c995190aef867217e5c06e6cd6662
madewithbill <165756396+madewithbill@users.noreply.github.com>
2025-07-19 20:58:48 -0700

    Adjusted config to remove trailing slashes. Changed base URL to include www subdomain for canonicals. Added noopener for external links.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 import sitemap from "@astrojs/sitemap";
+import { never } from "astro:schema";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://madewithbill.com",
+  site: "https://www.madewithbill.com",
+  trailingSlash: "never",
   vite: {
     plugins: [tailwindcss()],
   },

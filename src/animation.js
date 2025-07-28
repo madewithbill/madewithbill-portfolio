73207880c34bc34bee92e27670706e5872941eec
madewithbill <165756396+madewithbill@users.noreply.github.com>
2025-07-27 21:12:07 -0700

    Animation added to project route and about page. Tween adjusted as well.

--- src/animation.js
+++ src/animation.js
@@ -3,5 +3,5 @@ import { gsap } from "gsap";
 const chunk = document.getElementsByClassName('animate-gsap')
 
  document.addEventListener("DOMContentLoaded", (event) => {
-  gsap.from(chunk, { y: 20, opacity: 0, duration: .75, stagger: .2, autoAlpha: 0 })
+  gsap.from(chunk, { y: 20, opacity: 0, filter: "blur(5px)", duration: .5, stagger: .2, ease: "power1.out", autoAlpha: 0 })
  });
